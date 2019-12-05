88dc869585272446049af887785a2d6ba6792082
laichunx <48215216+laichunx@users.noreply.github.com>
2019-12-04 20:00:51 -0500

    upload
    upload

---
+++ Scripts/Bullet.gd
@@ -0,0 +1,21 @@
+extends Area
+
+var speed = 15
+var velocity = Vector3()
+var hurt = false
+
+func start(xform):
+	transform = xform
+	velocity = -transform.basis.z * speed
+	
+func _process(delta):
+	transform.origin += velocity * delta
+	
+func _on_Timer_timeout():
+	queue_free()
+
+func _on_Bullet2_body_entered(body):
+	if body.is_in_group("zombie"):
+		G.zombie.HP -= 10
+		G.zombie.set_state('hurt')
+		queue_free()
--- Scripts/Witch.gd
+++ Scripts/Witch.gd
@@ -2,18 +2,14 @@ extends KinematicBody
 
 onready var camera = $Pivot/Camera
 
-var HP = 100
 var gravity = -30
 var max_speed = 8
 var mouse_sensitivity = 0.002
+var HP = 100
 
 var velocity = Vector3()
 
 var Bullet = preload("res://Scenes/Bullet2.tscn")
-
-func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	G.player = self
 
 func get_input():
     var input_dir = Vector3()
@@ -33,7 +29,7 @@ func _unhandled_input(event):
     if event is InputEventMouseMotion:
         rotate_y(-event.relative.x * mouse_sensitivity)
         $Pivot.rotate_x(-event.relative.y * mouse_sensitivity)
-        $Pivot.rotation.x = clamp($Pivot.rotation.x, 0, 0)
+        $Pivot.rotation.x = clamp($Pivot.rotation.x, -1.2, 1.2)
 
     if event.is_action_pressed("attack"):
         var b = Bullet.instance()
@@ -42,11 +38,10 @@ func _unhandled_input(event):
         
 
 func _physics_process(delta):
-	velocity.y += gravity * delta
-	var desired_velocity = get_input() * max_speed
+    velocity.y += gravity * delta
+    var desired_velocity = get_input() * max_speed
 
-	velocity.x = desired_velocity.x
-	velocity.z = desired_velocity.z
-	velocity = move_and_slide(velocity, Vector3.UP, true)
-	if HP > 0 and HP < 0.5:
-		get_tree().change_scene("res://Scenes/End.tscn")
+    velocity.x = desired_velocity.x
+    velocity.z = desired_velocity.z
+    velocity = move_and_slide(velocity, Vector3.UP, true)
+	
